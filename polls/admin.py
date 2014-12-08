83cf5bfbcaf7770e2f072cde637a14be8d25f4db
Aaron1Alicia <aaron.wong@nyu.edu>
2014-12-07 23:08:50 -0500

    Admin page exploration

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    # specify how to group the properties of each question and how to display them
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+    # specify inline displayed items
+    inlines = [ChoiceInline]
+
+    # specify which fields will be displayed on admin page
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    # specify the field that is used to filter Question
+    list_filter = ['pub_date']
+
+    # specify which fields will be used for search
+    search_fields = ['question_text']
+
+
+
+# specify which model will be displayed on the admin page
+admin.site.register(Question, QuestionAdmin)
+
+#admin.site.register(Choice)
