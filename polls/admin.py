5db2899d1450a3732c7c104595fc56576716f9bc
Aaron1Alicia <aaron.wong@nyu.edu>
2014-12-07 21:54:58 -0500

    Tutorial 1 complted

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from polls.models import Question
 
 # Register your models here.
+
+admin.site.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import datetime
+
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,8 +10,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __unicode__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
