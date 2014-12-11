49f4fb4a0d73380a9b020ee605409e2fefda8905
Aaron1Alicia <aaron.wong@nyu.edu>
2014-12-10 20:55:56 -0500

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,12 +59,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 import dj_database_url
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
 
 
 DATABASES = {
@@ -99,9 +93,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
-
