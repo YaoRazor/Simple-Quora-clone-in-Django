33d6835e5c654934175c1ce687310e96eb409858
Aaron1Alicia <aaron.wong@nyu.edu>
2014-12-10 21:09:57 -0500

    try to make static files work

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,8 +72,8 @@ DATABASES = {
    }
 }
 
-#DATABASES['default'] = dj_database_url.config()
-#DATABASES['default']['ENGINE'] = 'django_postgrespool'
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
