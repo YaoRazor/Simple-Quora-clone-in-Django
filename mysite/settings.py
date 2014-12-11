da904da41b3615f4cae58bb26d3cfa5618a79e74
Aaron1Alicia <aaron.wong@nyu.edu>
2014-12-10 22:04:11 -0500

    fuck static

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,3 +100,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -13,6 +13,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# These statments are possibly useful when serve static files, skip for now
+#
 from dj_static import Cling
 application = Cling(get_wsgi_application())
