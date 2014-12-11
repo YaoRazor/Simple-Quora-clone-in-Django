b56246d492f64db79462b2085247ad67c2581356
Aaron1Alicia <aaron.wong@nyu.edu>
2014-12-10 22:00:59 -0500

    Heroku static files fixed

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,5 +14,5 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # These statments are possibly useful when serve static files, skip for now
-#from dj_static import Cling
-#application = Cling(get_wsgi_application())
+from dj_static import Cling
+application = Cling(get_wsgi_application())
