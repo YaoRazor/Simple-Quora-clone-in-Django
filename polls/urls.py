c5c0b47523dde3408ce27bb24f771346a2fbd222
Aaron1Alicia <aaron.wong@nyu.edu>
2014-12-12 23:27:04 -0500

    Refactor question detail, deperecate generic view

--- polls/urls.py
+++ polls/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    #url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    #url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
     # # ex: /polls/5/results/
     #url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
     url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
--- polls/views.py
+++ polls/views.py
@@ -18,11 +18,13 @@ def index(request):
     context = {'latest_question_list': latest_question_list, 'username': request.user.username}
     return render(request, 'polls/index.html', context)
 
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
 
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 class ResultsView(generic.DetailView):
